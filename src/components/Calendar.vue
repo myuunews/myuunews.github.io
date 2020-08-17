d774ddf1fa4bf98c481f351a52659774f9a5042e
theoria <theoria23@gmail.com>
2020-08-17 19:19:48 +0900

    Fullcalendarをやめた

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,85 +1,23 @@
 <template>
-  <ClientOnly>
-    <FullCalendar :options="calendarOptions" />
-  </ClientOnly>
+  <iframe
+    src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=Asia%2FTokyo&amp;src=cmI5aHFkMXV1aGc2dm05MWExOXRocWNubzRAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;src=NDc4Z2lqbDUzMnFya2tyY3UxYnJjZjE1aThAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;src=cXFvcW03N3Z0MzU0OGhrM28wbjI2Nmhwc2dAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23F09300&amp;color=%23009688&amp;color=%237986CB&amp;showTitle=1&amp;showNav=1&amp;showPrint=1&amp;showCalendars=1&amp;showTabs=1"
+    style="border-width:0"
+    width="100%"
+    height="600"
+    frameborder="0"
+    scrolling="no"
+  ></iframe>
 </template>
 
 <script>
-import FullCalendar from "@fullcalendar/vue";
-import dayGridPlugin from "@fullcalendar/daygrid";
-import timeGridPlugin from "@fullcalendar/timegrid";
-import interactionPlugin from "@fullcalendar/interaction";
-import googleCalendarPlugin from "@fullcalendar/google-calendar";
-// import jaLocale from "@fullcalendar/core/locales/ja";
-
 export default {
   name: "Calendar",
-  components: {
-    FullCalendar,
-  },
+  components: {},
   data() {
-    return {
-      calendarOptions: {
-        plugins: [
-          dayGridPlugin,
-          timeGridPlugin,
-          interactionPlugin,
-          googleCalendarPlugin,
-        ],
-        // locale: jaLocale,
-        initialView: "dayGridMonth",
-        height: "auto",
-        headerToolbar: {
-          left: "prev,next today",
-          center: "title",
-          right: "dayGridMonth,timeGridWeek,timeGridDay",
-        },
-        googleCalendarApiKey: process.env.GRIDSOME_GOOGLE_CAL_KEY,
-        eventSources: [
-          {
-            googleCalendarId:
-              "rb9hqd1uuhg6vm91a19thqcno4@group.calendar.google.com",
-            className: "gcal-event",
-            color: "#e65100",
-          },
-          {
-            googleCalendarId:
-              "qqoqm77vt3548hk3o0n266hpsg@group.calendar.google.com",
-            className: "gcal-onair",
-            color: "#26E",
-          },
-          {
-            googleCalendarId:
-              "478gijl532qrkkrcu1brcf15i8@group.calendar.google.com",
-            className: "canceled-event",
-            color: "black",
-          },
-        ],
-        eventClick: function(info) {
-          info.jsEvent.preventDefault();
-          if (info.event.url) {
-            window.open(info.event.url, "_blank");
-          }
-          // alert("Event: " + info.event.title);
-        },
-      },
-    };
+    return {};
   },
 };
 </script>
 
 <style>
-.fc a {
-  color: black;
-}
-.fc-daygrid-dot-event .fc-event-title {
-  white-space: normal;
-  display: -webkit-box;
-  -webkit-box-orient: vertical;
-  -webkit-line-clamp: 3;
-  overflow: hidden;
-}
-.fc-event-title {
-  font-weight: normal !important;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import DefaultLayout from "~/layouts/Default.vue";
 
-export default function(Vue, { router, head, isClient }) {
+export default function (Vue, {
+  router,
+  head,
+  isClient
+}) {
   Vue.component("Layout", DefaultLayout);
   head.script.push({
     src: "https://platform.twitter.com/widgets.js",
